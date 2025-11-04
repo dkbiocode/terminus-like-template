7d8c4ecd2542c44a8d77a0c3d3ab349158483f2e
David <davidcking.inbox@gmail.com>
2025-11-04 00:34:50 +0000

    finagling with container. works without messing with git hooks

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,10 @@
-sudo apt-get update
+#!/bin/bash
+set -e
+#sudo apt-get update
 
-sudo apt-get install -y git-lfs
+#sudo apt-get install -y git-lfs
 
-git lfs install
-
-echo 'source /workspaces/terminus-like-template/.game/wrappers.sh 2>/dev/null || true' >> ~/.bashrc"
+#git lfs update --force
+echo "Adding game wrappers to .bashrc..."
+echo 'source /workspaces/terminus-like-template/.game/wrappers.sh 2>/dev/null || true' >> ~/.bashrc
+echo "✅ Setup complete!"
